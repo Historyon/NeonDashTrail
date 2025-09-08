64c0a4687a06805659a350cb0d4a779070855999
Historyon <31914237+Historyon@users.noreply.github.com>
2025-09-09 00:33:47 +0200

    Resize length and heigth of wall run wall

--- entities/level_elements/WallRunWall.cs
+++ entities/level_elements/WallRunWall.cs
@@ -3,23 +3,26 @@ namespace NeonDashTrail.entities.level_elements;
 [Tool]
 public partial class WallRunWall : Area2D
 {
-    [Export] public int LengthInTiles { get => _lengthInTiles; set => UpdateWall(value, TileSize); }
-    [Export] public Vector2I TileSize { get => _tileSize; set => UpdateWall(LengthInTiles, value); }
-    [Export] public bool IsLeftWall { get; set; } = true;
+    [Export] public int LengthInTiles { get => _lengthInTiles; set => UpdateWall(value, HeightInTiles, TileSize); }
+    [Export] public int HeightInTiles { get => _heightInTiles; set => UpdateWall(LengthInTiles, value, TileSize); }
+    [Export] public Vector2I TileSize { get => _tileSize; set => UpdateWall(LengthInTiles, HeightInTiles, value); }
     [Export] private CollisionShape2D CollisionShape { get; set; }
     [Export] private Sprite2D Sprite { get; set; }
 
     private int _lengthInTiles = 1;
+    private int _heightInTiles = 1;
     private Vector2I _tileSize = new(16, 16);
 
-    private void UpdateWall(int lengthInTiles, Vector2I tileSize)
+    private void UpdateWall(int lengthInTiles, int heightInTiles, Vector2I tileSize)
     {
         if (Sprite is null || CollisionShape is null) return;
 
         _lengthInTiles = lengthInTiles;
+        _heightInTiles = heightInTiles;
         _tileSize = tileSize;
 
-        Sprite.RegionRect = new Rect2I(Vector2I.Zero, new(_tileSize.X * _lengthInTiles, _tileSize.Y));
-        CollisionShape.Shape = new RectangleShape2D { Size = new Vector2(_tileSize.X * _lengthInTiles, _tileSize.Y) };
+        Sprite.RegionRect = new Rect2I(Vector2I.Zero, new(_tileSize.X * _lengthInTiles, _tileSize.Y * _heightInTiles));
+        CollisionShape.Shape = new RectangleShape2D { Size = new Vector2(_tileSize.X * _lengthInTiles, _tileSize.Y * _heightInTiles) };
+        CollisionShape.Position = new Vector2(_tileSize.X * _lengthInTiles / 2f - _tileSize.X / 2f , _tileSize.Y * _heightInTiles / 2f - _tileSize.Y / 2f); 
     }
 }
